2e017aee9cd2e4ed7cbe858d8c9dab472e6c77b1
Brad Clawsie <brad@b7j0c.org>
2023-02-25 18:33:55 -0600

    added

---
+++ test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "https://deno.land/std@0.178.0/testing/asserts.ts";
+import {
+  bytesToHex,
+  bytesToString,
+  hexToBytes,
+  stringToBytes,
+} from "./mod.ts";
+
+Deno.test("round trip hex value", () => {
+  const s = "hello world";
+  const h = bytesToHex(stringToBytes(s));
+  assertEquals(h, "68656c6c6f20776f726c64", "hex value");
+  assertEquals(bytesToString(hexToBytes(h)), s, "round trip");
+});
